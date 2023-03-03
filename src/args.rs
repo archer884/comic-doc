31fe3dac441add20b1aae189a3c71ba101ed23c1
JMA <archer884@zoho.com>
2023-03-03 12:57:27 -0600

    First cut

--- src/args.rs
+++ src/args.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use clap::Parser;
 
@@ -51,8 +51,25 @@ impl Args {
 #[derive(Debug)]
 pub struct ProcessingContext<'a> {
     /// overwrite existing files or directories
-    force: bool,
+    pub force: bool,
 
     /// output directory
     target: Option<&'a str>,
+}
+
+impl ProcessingContext<'_> {
+    pub fn output_path(&self, path: &Path) -> PathBuf {
+        // Some asshole sent us a file.
+        if !path.is_dir() {
+            panic!("can't derive output path for non-directory");
+        }
+
+        match self.target {
+            Some(target) => {
+                let name = path.file_name().expect("must not be root directory");
+                Path::new(target).join(name).with_extension("cbz")
+            }
+            None => path.with_extension("cbz"),
+        }
+    }
 }
--- src/item.rs
+++ src/item.rs
@@ -1,10 +1,10 @@
 use std::{
-    io,
-    path::{Path, PathBuf}, fs,
+    ffi::OsStr,
+    fs, io,
+    path::{Path, PathBuf},
 };
 
 use tar::Builder;
-use walkdir::WalkDir;
 
 use crate::args::ProcessingContext;
 
@@ -26,74 +26,108 @@ impl Item {
     }
 }
 
-fn create_tarball(context: &ProcessingContext, path: &Path) -> io::Result<()> {
-    let walker = WalkDir::new(path)
-        .into_iter()
-        .filter_map(|x| x.ok());
+struct PathMeta {
+    path: PathBuf,
+    metadata: fs::Metadata,
+}
 
+impl PathMeta {
+    fn new(source: impl PathMetaSource) -> io::Result<Self> {
+        source.into()
+    }
+}
+
+trait PathMetaSource {
+    fn into(self) -> io::Result<PathMeta>;
+}
+
+impl PathMetaSource for fs::DirEntry {
+    fn into(self) -> io::Result<PathMeta> {
+        Ok(PathMeta {
+            metadata: self.metadata()?,
+            path: self.path(),
+        })
+    }
+}
+
+fn create_tarball(context: &ProcessingContext, path: &Path) -> io::Result<()> {
     let mut archive = Builder::new(Vec::new());
-    
-    
-    todo!()
+
+    populate_archive(path, &mut archive)?;
+
+    let path = context.output_path(path);
+    if path.exists() && !context.force {
+        return Err(io::Error::new(
+            io::ErrorKind::AlreadyExists,
+            "intended path exists",
+        ));
+    }
+
+    fs::write(path, archive.into_inner()?)
 }
 
 fn repair_tarball(context: &ProcessingContext, path: &Path) -> io::Result<()> {
-    todo!()
+    todo!("We might never implement this...")
 }
 
-struct ArchiveObject {
-    name: String,
-    subdirs: Vec<ArchiveObject>,
-    files: Vec<BinaryObject>,
+// PLAN: This function will recurse down each path. The level of recursion will
+// be sharply limited, because these files were created by a human, and
+// recursion will only occur in the even of a legitimate need.
+//
+// SPECIFICALLY: webp subdirectories will NOT incur recursion, because the
+// contents of those will be archived IN LIEU of the contents of the parent.
+fn populate_archive<T>(path: &Path, archive: &mut Builder<T>) -> io::Result<()>
+where
+    T: io::Write,
+{
+    let (files, directories) = read_level(path)?;
+
+    // If we have a webp subdirectory, we're probably going to completely
+    // ignore these files.
+
+    if let Some(webp) = get_webp_dir(&directories) {
+        let files = fs::read_dir(&webp.path)?.filter_map(|entry| {
+            let entry = entry.ok()?;
+            entry.file_type().ok()?.is_file().then(|| entry.path())
+        });
+        append_files(archive, files)?;
+        return Ok(());
+    }
+
+    // We apparently DON'T have a webp directory, so we're going to continue.
+
+    append_files(archive, files.into_iter().map(|meta| meta.path))?;
+
+    for dir in directories {
+        populate_archive(&dir.path, archive)?;
+    }
+
+    Ok(())
 }
 
+fn append_files<T, I>(archive: &mut Builder<T>, files: I) -> io::Result<()>
+where
+    T: io::Write,
+    I: IntoIterator<Item = PathBuf>,
+{
+    for path in files {
+        let mut file = fs::File::open(&path)?;
+        archive.append_file(path.file_name().unwrap(), &mut file)?;
+    }
+    Ok(())
+}
 
+fn read_level(path: &Path) -> io::Result<(Vec<PathMeta>, Vec<PathMeta>)> {
+    Ok(fs::read_dir(path)?
+        .filter_map(|entry| PathMeta::new(entry.ok()?).ok())
+        .partition(|meta| meta.metadata.is_file()))
+}
 
-impl ArchiveObject {
-    fn new(path: &Path) -> io::Result<Self> {
-        let mut dirs = Vec::new();
-        let mut files = Vec::new();
-
-        for candidate in fs::read_dir(path)? {
-            let entry = candidate?;
-            let info = entry.metadata()?;
-
-            if info.is_dir() {
-                dirs.push(ArchiveObject::new(&entry.path())?);
-            }
-
-            if info.is_file() {
-                files.push(BinaryObject::from_path)
-            }
-
-
-
-            if entry
+fn get_webp_dir(dirs: &[PathMeta]) -> Option<&PathMeta> {
+    if let [dir] = dirs {
+        if dir.path.ends_with(OsStr::new("webp")) {
+            return Some(dir);
         }
-
-        let (files, dirs) = fs::read_dir(path)?
-            .filter_map(|entry| {
-                let entry = entry.ok()?;
-                let meta = entry.metadata().ok()?;
-                Some((entry.path(), meta))
-            })
-            .partition(|pair| pair.1.is_file());
-
-        todo!()
-            
     }
-}
-
-struct BinaryObject {
-    name: String,
-    data: Vec<u8>,
-}
-
-impl BinaryObject {
-    fn new(path: &Path) -> io::Result<Self> {
-        let name = path
-            .file_name()
-            .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "not a file"))?;
-
-    }
+    None
 }
